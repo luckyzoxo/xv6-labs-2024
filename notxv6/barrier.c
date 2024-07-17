f593ec4d79cc16a78079a6cfeeaa504423a3e391
luckyzoxo <2545411681@qq.com>
2024-07-17 10:50:20 +0800

    Lab: Multithreading

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -6,12 +6,14 @@
 
 static int nthread = 1;
 static int round = 0;
+pthread_mutex_t lock;
 
-struct barrier {
+struct barrier
+{
   pthread_mutex_t barrier_mutex;
   pthread_cond_t barrier_cond;
-  int nthread;      // Number of threads that have reached this round of the barrier
-  int round;     // Barrier round
+  int nthread; // Number of threads that have reached this round of the barrier
+  int round;   // Barrier round
 } bstate;
 
 static void
@@ -22,7 +24,13 @@ barrier_init(void)
   bstate.nthread = 0;
 }
 
-static void 
+static void barrier_destroy(void)
+{
+  assert(pthread_mutex_destroy(&bstate.barrier_mutex) == 0);
+  assert(pthread_cond_destroy(&bstate.barrier_cond) == 0);
+}
+
+static void
 barrier()
 {
   // YOUR CODE HERE
@@ -30,19 +38,31 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+
+  if (++bstate.nthread < nthread)
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  else
+  {
+    bstate.nthread = 0;
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
 thread(void *xa)
 {
-  long n = (long) xa;
-  long delay;
+  // long n = (long)xa;
+  // long delay;
   int i;
 
-  for (i = 0; i < 20000; i++) {
+  for (i = 0; i < 20000; i++)
+  {
     int t = bstate.round;
-    assert (i == t);
+    assert(i == t);
     barrier();
     usleep(random() % 100);
   }
@@ -50,15 +70,16 @@ thread(void *xa)
   return 0;
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   pthread_t *tha;
   void *value;
   long i;
-  double t1, t0;
+  // double t1, t0;
+  pthread_mutex_init(&lock, NULL);
 
-  if (argc < 2) {
+  if (argc < 2)
+  {
     fprintf(stderr, "%s: %s nthread\n", argv[0], argv[0]);
     exit(-1);
   }
@@ -68,11 +89,16 @@ main(int argc, char *argv[])
 
   barrier_init();
 
-  for(i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, thread, (void *) i) == 0);
+  for (i = 0; i < nthread; i++)
+  {
+    assert(pthread_create(&tha[i], NULL, thread, (void *)i) == 0);
   }
-  for(i = 0; i < nthread; i++) {
+  for (i = 0; i < nthread; i++)
+  {
     assert(pthread_join(tha[i], &value) == 0);
   }
+
+  barrier_destroy();
+
   printf("OK; passed\n");
 }
